7049c6d8830dd84e3f907cca7bc487e7616d8c91
Hanumanthumanoj01 <109654916+Hanumanthumanoj01@users.noreply.github.com>
2022-07-27 13:19:52 +0530

    JavaProject

---
+++ Implicitexplicit.java
@@ -0,0 +1,25 @@
+package typecasting;
+public class Implicitexplicit {
+public static void main(String[] args) {		
+		//implicit conversion
+		System.out.println("Implicit Type Casting");
+		char a='K';
+		System.out.println("Value of a: "+a);		
+		int b=a;
+		System.out.println("Value of b: "+b);		
+		float c=a;
+		System.out.println("Value of c: "+c);		
+		long d=a;
+		System.out.println("Value of d: "+d);		
+		double e=a;
+		System.out.println("Value of e: "+e);				
+		System.out.println("\n");		
+		System.out.println("Explicit Type Casting");
+		//explicit conversion		
+		double x=65.5;
+		int y=(int)x;
+		System.out.println("Value of x: "+x);
+		System.out.println("Value of y: "+y);
+		
+	}
+}
